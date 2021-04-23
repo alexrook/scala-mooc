ae6679d765b7b279fc2950c6ae42a0dd7a57bec5
Alex Grachev <aleksandr.grachev@fv.dev>
2021-04-23 17:28:20 +0300

    task_caesar добавлены реализации методов encrypt и decrypt

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -4,6 +4,11 @@ import homeworks.HomeworksUtils.TaskSyntax
 
 object task_caesar {
 
+
+  val Alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  class CipherException(msg: String) extends Exception(msg)
+
   /**
    * В данном задании Вам предлагается реализовать функции,
    * реализующие кодирование/декодирование строки шифром Цезаря.
@@ -12,20 +17,53 @@ object task_caesar {
    * Сдвиг   - неотрицательное целое число.
    * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
    */
+
   /**
    * @param word   входное слово, которое необходимо зашифровать
    * @param offset сдвиг вперёд по алфавиту
    * @return зашифрованное слово
    */
-  def encrypt(word: String, offset: Int): String =
-    task"Реализуйте метод `encrypt`"()
+  def encrypt(word: String, offset: Int): String = {
+    val newOffset: Int = offset % Alphabet.length
+    word.foldLeft("") {
+      case (acc: String, char: Char) =>
+        val idx: Int = Alphabet.indexOf(char)
+        if (idx > -1) {
+          val newChar: Char = if ((idx + newOffset) > (Alphabet.length - 1)) {
+            val ni = (idx + newOffset) - Alphabet.length
+            Alphabet(ni)
+          } else {
+            Alphabet(idx + newOffset)
+          }
+          acc :+ newChar
+        } else {
+          throw new CipherException(s"unsupported alphabet[$char]")
+        }
+    }
+  }
 
   /**
    * @param cipher шифр, который необходимо расшифровать
    * @param offset сдвиг вперёд по алфавиту
    * @return расшифрованное слово
    */
-  def decrypt(cipher: String, offset: Int): String =
-    task"Реализуйте метод `decrypt`"()
+  def decrypt(cipher: String, offset: Int): String = {
+    val newOffset: Int = offset % Alphabet.length
+    cipher.foldLeft("") {
+      case (acc: String, char: Char) =>
+        val idx: Int = Alphabet.indexOf(char)
+        if (idx > -1) {
+          val newChar: Char = if ((idx - newOffset) < 0) {
+            val ni = Alphabet.length + (idx - newOffset)
+            Alphabet(ni)
+          } else {
+            Alphabet(idx - newOffset)
+          }
+          acc :+ newChar
+        } else {
+          throw new CipherException(s"unsupported alphabet[$char]")
+        }
+    }
+  }
 
 }
